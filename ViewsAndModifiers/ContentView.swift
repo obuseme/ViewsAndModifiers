81aa549c7d0ed68808a8a3a97c12063be53f7795
obuseme <obusek@gmail.com>
2020-07-14 08:08:16 -0400

    Initial Commit

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Andrew Obusek on 7/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
